0006e52a7a4c808203971665d1d6785147494b6f
shige1114 <76639625+shige1114@users.noreply.github.com>
2024-01-23 15:30:06 +0900

    Update __init__.py
    to publick

--- 実験/back/db/__init__.py
+++ 実験/back/db/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 import os
 
-DB_URL = os.getenv("DB_URL","postgresql://root:password@db:5432/test_db")
+DB_URL = os.getenv("DB_URL")
 print(DB_URL)
 engine = create_engine(DB_URL,echo=False)
 db_session = scoped_session(
